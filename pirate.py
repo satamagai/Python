6fc026ba33848ca6999d99ad41963ce6375e5bcf
Sat Amagai <satamagai@amagais-hqlm1.hhmi.org>
2017-01-20 11:22:59 -0500

    Create a simple treasure map

--- pirate.py
+++ pirate.py
@@ -3,4 +3,9 @@ for i in range(100):
 	print(str(i) + "bottles of rum")
 with open('treasure.txt') as fh:
   for line in fh:
-    print line
+    print(line)
+
+with open('map.txt', 'w') as fh:
+  for i in range(10):
+    fh.write("Walk " + str(i) + " paces left.")
+
